9d7fdb23447479c9eaa72085b91e8c6e020398f0
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-21 16:56:03 -0300

    Adiciona auth com Google

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,15 @@ module.exports = {
   },
   rules: {
     'no-console': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-/* eslint-disable no-param-reassign */
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithRedirect,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 
 import Router from 'vanilla-router';
 
 import './index.scss';
+import './results.scss';
 
 import startPageMarkup from './start.html';
 import resultsPageMarkup from './results.html';
-import './results.scss';
+import userPageMarkup from './user.html';
+import configPageMarkup from './config.html';
+
+// Objetos globais
 
 const app = initializeApp({
   apiKey: 'AIzaSyBCt2DDVqDSBatSX0vZ0AE_VTGXXHmg4uE',
@@ -24,22 +34,84 @@ const app = initializeApp({
   measurementId: 'G-9YQZ9JT5DE',
 });
 
-// eslint-disable-next-line no-unused-vars
-const analytics = getAnalytics(app);
-const db = getFirestore();
-
-const getResults = async () => {
-  const docRef = doc(db, 'results', '1');
-  const docSnap = await getDoc(docRef);
-  return docSnap.exists() ? docSnap.data() : {};
-};
-
 const appRouter = new Router({
   mode: 'history',
   root: '/',
 });
 
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+const db = getFirestore();
+const auth = getAuth(app);
+
+let user;
+
 const contentElement = document.getElementById('content');
+
+// UI helpers
+
+const hideLoginControls = () => {
+  contentElement.querySelectorAll('.logado').forEach((el) => {
+    const elemento = el;
+    elemento.style.display = 'none';
+  });
+  contentElement.querySelectorAll('.nao-logado').forEach((el) => {
+    const elemento = el;
+    elemento.style.display = 'none';
+  });
+};
+
+const loadSpinner = (show = true) => {
+  contentElement.querySelectorAll('.carregando-auth').forEach((el) => {
+    const elemento = el;
+    elemento.style.display = show ? 'block' : 'none';
+  });
+  hideLoginControls();
+};
+
+const showLogged = (show = true) => {
+  if (show) {
+    contentElement.querySelectorAll('.logado').forEach((el) => {
+      const elemento = el;
+      elemento.style.display = 'block';
+    });
+    contentElement.querySelectorAll('.nao-logado').forEach((el) => {
+      const elemento = el;
+      elemento.style.display = 'none';
+    });
+  } else {
+    contentElement.querySelectorAll('.logado').forEach((el) => {
+      const elemento = el;
+      elemento.style.display = 'none';
+    });
+    contentElement.querySelectorAll('.nao-logado').forEach((el) => {
+      const elemento = el;
+      elemento.style.display = 'block';
+    });
+  }
+};
+
+const updateAuthControls = () => {
+  const knownState = user !== undefined;
+
+  loadSpinner(!knownState);
+  if (knownState) showLogged(user);
+};
+
+// Autenticação
+
+const googleRedirectLogin = (event) => {
+  event.preventDefault();
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider);
+};
+
+const logout = (event) => {
+  event.preventDefault();
+  signOut(auth);
+};
+
+// Navegação
 
 const handleNavigate = (event) => {
   event.preventDefault();
@@ -48,9 +120,16 @@ const handleNavigate = (event) => {
 };
 
 const updateListeners = () => {
-  contentElement.querySelectorAll('a').forEach((a) => {
+  const links = contentElement.querySelectorAll('a.route');
+  links?.forEach((a) => {
     a.addEventListener('click', handleNavigate);
   });
+
+  const loginGoogle = contentElement.querySelector('.login-google');
+  loginGoogle?.addEventListener('click', googleRedirectLogin);
+
+  const logoutButton = contentElement.querySelector('.logout');
+  logoutButton?.addEventListener('click', logout);
 };
 
 const show = (markup) => {
@@ -58,12 +137,31 @@ const show = (markup) => {
   updateListeners();
 };
 
-const showStartPage = () => {
-  show(startPageMarkup);
+// Serviços
+
+const getResults = async () => {
+  const docRef = doc(db, 'results', '1');
+  const docSnap = await getDoc(docRef);
+  return docSnap.exists() ? docSnap.data() : {};
 };
 
-const showLoginPage = () => {
-  show('login');
+// Páginas
+
+const showStartPage = () => {
+  show(startPageMarkup);
+  updateAuthControls(user);
+};
+
+const showUserPage = () => {
+  if (user) {
+    show(userPageMarkup);
+  }
+};
+
+const showConfigPage = () => {
+  if (user) {
+    show(configPageMarkup);
+  }
 };
 
 const showResultsPage = () => {
@@ -76,14 +174,18 @@ const showResultsPage = () => {
 
     show(resultsPageMarkup);
 
-    const biscoitoPercentual = document.querySelector('#biscoito .percentual');
-    const bolachaPercentual = document.querySelector('#bolacha .percentual');
+    const biscoitoPercentual = contentElement.querySelector(
+      '#biscoito .percentual'
+    );
+    const bolachaPercentual = contentElement.querySelector(
+      '#bolacha .percentual'
+    );
 
     biscoitoPercentual.innerHTML = `${resultado.biscoito100.toFixed(1)}%`;
     bolachaPercentual.innerHTML = `${resultado.bolacha100.toFixed(1)}%`;
 
-    const biscoito = document.querySelector('#biscoito');
-    const bolacha = document.querySelector('#bolacha');
+    const biscoito = contentElement.querySelector('#biscoito');
+    const bolacha = contentElement.querySelector('#bolacha');
 
     biscoito.style.fontSize = `${1 + resultado.biscoito}em`;
     bolacha.style.fontSize = `${1 + resultado.bolacha}em`;
@@ -103,10 +205,24 @@ const show404 = () => {
   show('404');
 };
 
+// Inicialização
+
 appRouter
   .add('/', showStartPage)
-  .add('/login', showLoginPage)
+  .add('/user', showUserPage)
+  .add('/config', showConfigPage)
   .add('/results', showResultsPage)
   .add(':any', show404)
   .addUriListener()
   .navigateTo('/');
+
+document.addEventListener('DOMContentLoaded', () => {
+  updateAuthControls();
+
+  onAuthStateChanged(auth, (loggedUser) => {
+    user = loggedUser;
+    updateAuthControls();
+  });
+
+  updateListeners();
+});
